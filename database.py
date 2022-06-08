3ed0fd09a78c3d0072511b41a5a6fc8b2736deec
joelruizcabrera <joel.schwegmann@gmx.de>
2022-06-08 11:32:56 +0200

    init develop branch

--- database.py
+++ database.py
@@ -1,26 +1,21 @@
 import mysql
 import mysql.connector
 
-#Verbindung zur MySQL Datenbank herstellen
+# SQL Connection try
 db = mysql.connector.connect(host = "127.0.0.1",
                                     user = "root",
                                     passwd = "",
                                     db = "hangman")
 
-#Cursor zur Ausführung von SQL Statements einrichten
-cursor = db.cursor()
+# Fetching data
+stmt = db.cursor()
+stmt.execute("SELECT * from hang_words")
+results = stmt.fetchall()
 
-#Ausführen von SQL Statements
-cursor.execute("SELECT * from Test")
+stmt.close()
 
-#Auslesen von Daten und Speichern in einer Variable
-ergebnis = cursor.fetchall()
-
-cursor.close()
-
-#Ausgabe der Daten aus der Variable
-for data in ergebnis:
-
-    print ("Nummer: " + str(data[0]) + "; Text: " + data[1])
+# Go into words
+for hangmanWord in results:
+    print ("ID: " + str(hangmanWord[0]) + "\nSchwierigkeit: " + str(hangmanWord[1]) + "\nWort: " + hangmanWord[2] + "\n------------------------------------")
 
 db.close()
